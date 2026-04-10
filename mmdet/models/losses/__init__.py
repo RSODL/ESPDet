d3ab9d5b779811dceb9ecdf4a8f9e65d475f6b59
RSODL <155142251+RSODL@users.noreply.github.com>
2026-04-10 20:16:57 +0800

    Update __init__.py

--- mmdet/models/losses/__init__.py
+++ mmdet/models/losses/__init__.py
@@ -12,7 +12,7 @@ from .focal_loss import FocalCustomLoss, FocalLoss, sigmoid_focal_loss
 from .gaussian_focal_loss import GaussianFocalLoss
 from .gfocal_loss import DistributionFocalLoss, QualityFocalLoss
 from .ghm_loss import GHMC, GHMR
-from .iou_loss import (BoundedIoULoss, CIoULoss, DIoULoss, EIoULoss, GIoULoss,
+from .iou_loss import (BoundedIoULoss, CIoULoss, DIoULoss, EIoULoss, GIoULoss,FocalerGIoULoss,
                        IoULoss, SIoULoss, bounded_iou_loss, iou_loss)
 from .kd_loss import KnowledgeDistillationKLDivLoss
 from .l2_loss import L2Loss
@@ -31,7 +31,7 @@ __all__ = [
     'mask_cross_entropy', 'CrossEntropyLoss', 'sigmoid_focal_loss',
     'FocalLoss', 'smooth_l1_loss', 'SmoothL1Loss', 'balanced_l1_loss',
     'BalancedL1Loss', 'mse_loss', 'MSELoss', 'iou_loss', 'bounded_iou_loss',
-    'IoULoss', 'BoundedIoULoss', 'GIoULoss', 'DIoULoss', 'CIoULoss',
+    'IoULoss', 'BoundedIoULoss', 'GIoULoss', 'DIoULoss', 'CIoULoss','FocalerGIoULoss',
     'EIoULoss', 'SIoULoss', 'GHMC', 'GHMR', 'reduce_loss',
     'weight_reduce_loss', 'weighted_loss', 'L1Loss', 'l1_loss', 'isr_p',
     'carl_loss', 'AssociativeEmbeddingLoss', 'GaussianFocalLoss',
